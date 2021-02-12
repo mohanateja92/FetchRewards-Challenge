7fe92bbe1c7db60c6a5f2c9dd1434d7c6f1f9e25
Mohana G <srigorti@Sris-MBP.attlocal.net>
2021-02-12 00:01:02 -0600

    Customized heart shaped favorite Button using Beizerpath and IBInspectable and IBDesignable

---
+++ FetchRewardsEvents/View/HeartButton.swift
@@ -0,0 +1,37 @@
+//
+//  HeartButton.swift
+//  FetchRewardsEvents
+//
+//  Created by Mohana G on 2/11/21.
+//
+
+import UIKit
+
+@IBDesignable class HeartButton: UIButton {
+
+    @IBInspectable var filled: Bool = true {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+@IBInspectable var strokeWidth: CGFloat = 2.0
+@IBInspectable var strokeColor: UIColor?
+
+override func draw(_ rect: CGRect) {
+    let bezierPath = UIBezierPath(heartIn: self.bounds)
+
+    if self.strokeColor != nil {
+        self.strokeColor!.setStroke()
+    } else {
+        self.tintColor.setStroke()
+    }
+
+    bezierPath.lineWidth = self.strokeWidth
+    bezierPath.stroke()
+
+    if self.filled {
+        self.tintColor.setFill()
+        bezierPath.fill()
+    }
+}
+}
